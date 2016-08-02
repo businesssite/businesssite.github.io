dbfce05a7702fc253554ca34c093988d48659fab
VitalikSt <vitalik.sts@gmail.com>
2016-08-02 11:54:54 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/***********Touch***************/
+$(document).ready(function(){
+	$('#touch').click(function(){
+		$('nav ul').slideToggle('slow');
+	});
+
+});
+/************//**************/
+
+/************Animate*********/
+$(document).ready(function(){
+$('.item').addClass("hidden-box").viewportChecker({
+	classToAdd: 'visible-box animated zoomIn'
+	});
+$('.one').addClass("hidden-box").viewportChecker({
+	classToAdd: 'visible-box animated fadeInLeft'
+	});
+$('.two').addClass("hidden-box").viewportChecker({
+	classToAdd: 'visible-box animated fadeInRight'
+	});
+$('.three').addClass("hidden-box").viewportChecker({
+	classToAdd: 'visible-box animated fadeInUp'
+	});
+$('.four').addClass("hidden-box").viewportChecker({
+	classToAdd: 'visible-box animated bounceIn'
+	});
+$('.forma, #triangle-up').addClass("hidden-box").viewportChecker({
+	classToAdd: 'visible-box animated flipInX'
+	});
+});  //end document animate
+/************//**************/
+
+/*******.Register forma*********/
+$(document).ready(function(){
+	$(".forma input").focusin(function() {
+		$(this).css({border:'2px solid #1287bf'});
+	});
+	$(".forma input").focusout(function() {
+		$(this).css({border:'2px solid #d8d8d8'});
+	});
+});
+/************//**************/
